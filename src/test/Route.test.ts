7d096394d0eafda7eca153938aa29dd95618657a
ploiu <curtislanglitz+github@gmail.com>
2021-03-25 21:17:58 -0400

    more tests for a method I have yet to implement

--- src/test/Route.test.ts
+++ src/test/Route.test.ts
@@ -1,114 +1,258 @@
 import {
-	assert,
-	assertNotEquals,
-	assertEquals
+  assert,
+  assertEquals,
+  assertNotEquals,
 } from "https://deno.land/std@0.91.0/testing/asserts.ts";
 import Route from "../ts/request/Route.ts";
 
 Deno.test("properly parses path variables", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		// adding a query variable after an optional variable to make sure that works
-		url: "/test/:name/:age??:a",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.hasPathVariable("name", false), 'route has name as mandatory variable');
-	assert(route.hasPathVariable("age", true), 'route has age as optional variable');
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    // adding a query variable after an optional variable to make sure that works
+    url: "/test/:name/:age??:a",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.hasPathVariable("name", false),
+    "route has name as mandatory variable",
+  );
+  assert(
+    route.hasPathVariable("age", true),
+    "route has age as optional variable",
+  );
 });
 
 Deno.test("properly parses query variables", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		// adding a query variable after an optional variable to make sure that works
-		url: "/test/?:name&:age?",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.hasQueryVariable("name", false), 'route has name as mandatory variable');
-	assert(route.hasQueryVariable("age", true), 'route has age as optional variable');
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    // adding a query variable after an optional variable to make sure that works
+    url: "/test/?:name&:age?",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.hasQueryVariable("name", false),
+    "route has name as mandatory variable",
+  );
+  assert(
+    route.hasQueryVariable("age", true),
+    "route has age as optional variable",
+  );
 });
 
 Deno.test("doesUrlMatch matches simple url", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch("/test"), 'url should match /test');
-	// make sure other routes starting with /test don't match
-	assertNotEquals(route.doesUrlMatch("/test/a/b/c"), true, 'url should not match /test/a/b/c');
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(route.doesUrlMatch("/test"), "url should match /test");
+  // make sure other routes starting with /test don't match
+  assertNotEquals(
+    route.doesUrlMatch("/test/a/b/c"),
+    true,
+    "url should not match /test/a/b/c",
+  );
 });
 
 Deno.test("doesUrlMatch matches simple url with multiple path sections", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test/a/b/c",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch("/test/a/b/c"), 'url should match /test/a/b/c');
-	// ensure other routes that don't match aren't counted
-	assertNotEquals(route.doesUrlMatch("/test"), true, 'url should not match /test');
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/a/b/c",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(route.doesUrlMatch("/test/a/b/c"), "url should match /test/a/b/c");
+  // ensure other routes that don't match aren't counted
+  assertNotEquals(
+    route.doesUrlMatch("/test"),
+    true,
+    "url should not match /test",
+  );
 });
 
 Deno.test("doesUrlMatch matches url with path variables", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test/:name/:age",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch("/test/ploiu/23"), 'url should match with both variables filled in');
-	assertNotEquals(route.doesUrlMatch("/test/ploiu"), true, 'url should not match without missing variables');
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/:name/:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.doesUrlMatch("/test/ploiu/23"),
+    "url should match with both variables filled in",
+  );
+  assertNotEquals(
+    route.doesUrlMatch("/test/ploiu"),
+    true,
+    "url should not match without missing variables",
+  );
 });
 
 Deno.test("doesUrlMatch matches url with optional path variables", () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test/:name?/:age",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch("/test/ploiu/23"), 'url should match with all variables');
-	assert(route.doesUrlMatch("/test/23"), 'url should match with one variable');
-	assertNotEquals(route.doesUrlMatch('/test'), true, 'url should not match without any variables')
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/:name?/:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.doesUrlMatch("/test/ploiu/23"),
+    "url should match with all variables",
+  );
+  assert(route.doesUrlMatch("/test/23"), "url should match with one variable");
+  assertNotEquals(
+    route.doesUrlMatch("/test"),
+    true,
+    "url should not match without any variables",
+  );
 });
 
-Deno.test('doesUrlMatch matches url with query variables', () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test?:name&:age",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch('/test?name=ploiu&age=23'), 'url should match with both variables')
-	assertNotEquals(route.doesUrlMatch('/test?name=ploiu'), true, 'url should not match without all variables')
-})
-Deno.test('doesUrlMatch matches url with optional query variables', () => {
-	const route = Route.fromObject({
-		title: "test",
-		method: "GET",
-		url: "/test?:name?&:age",
-		accept: "*",
-		responseHeaders: {},
-		response: "hi",
-	});
-	assert(route.doesUrlMatch('/test?name=ploiu&age=23'), 'url should match with all variables')
-	assert(route.doesUrlMatch('/test?age=23'), 'url should match with one variable')
-})
+Deno.test("doesUrlMatch matches url with query variables", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test?:name&:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.doesUrlMatch("/test?name=ploiu&age=23"),
+    "url should match with both variables",
+  );
+  assertNotEquals(
+    route.doesUrlMatch("/test?name=ploiu"),
+    true,
+    "url should not match without all variables",
+  );
+});
+Deno.test("doesUrlMatch matches url with optional query variables", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test?:name?&:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.doesUrlMatch("/test?name=ploiu&age=23"),
+    "url should match with all variables",
+  );
+  assert(
+    route.doesUrlMatch("/test?age=23"),
+    "url should match with one variable",
+  );
+});
+
+Deno.test("doesUrlMatch matches url with mandatory and optional path and query parameters", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/:name/:age??:favoriteColor&:favoriteFood?",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assert(
+    route.doesUrlMatch("/test/ploiu/23?favoriteColor=green&favoriteFood=pasta"),
+    "url should match with all variables",
+  );
+  assert(
+    route.doesUrlMatch("/test/ploiu?favoriteColor=green"),
+    "url should match with only mandatory variables",
+  );
+});
+
+Deno.test("parseVariablesFromUrl should return an empty js object if there are no variables to parse", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/a/b/c",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+  assertEquals(route.parseVariablesFromUrl("/test/a/b/c"), {});
+});
+
+Deno.test("parseVariablesFromUrl should return an object with path variables in it", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/:name/:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+
+  assertEquals(route.parseVariablesFromUrl("/test/ploiu/23"), {
+    name: "ploiu",
+    age: "23",
+  }, "path parameters should be included");
+});
+
+Deno.test("parseVariablesFromUrl should set non-included path variables as null", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test/:name?/:age/:favoriteFood?",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+
+  assertEquals(route.parseVariablesFromUrl("/test/23"), {
+    name: null,
+    age: "23",
+    favoriteFood: null,
+  }, "optional path variables not included should be set to null");
+});
+
+Deno.test("parseVariablesFromUrl should include query parameters", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test?:name&:age",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+
+  assertEquals(route.parseVariablesFromUrl("/test?name=ploiu&age=23"), {
+    name: null,
+    age: "23",
+  }, "query parameters should be included");
+});
+
+Deno.test("parseVariablesFromUrl should set non-included path variables as null", () => {
+  const route = Route.fromObject({
+    title: "test",
+    method: "GET",
+    url: "/test?:name?&:age&:favoriteFood?",
+    accept: "*",
+    responseHeaders: {},
+    response: "hi",
+  });
+
+  assertEquals(route.parseVariablesFromUrl("/test?age=23"), {
+    name: null,
+    age: "23",
+    favoriteFood: null,
+  }, "optional query variables not included should be set to null");
+});
--- src/ts/request/Route.ts
+++ src/ts/request/Route.ts
@@ -1,151 +1,165 @@
-import {RequestMethod} from "./RequestMethod.ts";
+import { RequestMethod } from "./RequestMethod.ts";
 import UrlVariable from "./UrlVariable.ts";
 
 export default class Route {
-	public matchedParts: any = {};
-	public pathVariables: UrlVariable[] = [];
-	public queryVariables: UrlVariable[] = [];
-	private compiledUrlRegex: RegExp;
+  public matchedParts: any = {};
+  public pathVariables: UrlVariable[] = [];
+  public queryVariables: UrlVariable[] = [];
+  private compiledUrlRegex: RegExp;
 
-	constructor(
-		public title: string,
-		public url: string,
-		public method: RequestMethod,
-		public accept: string,
-		public responseHeaders: Headers,
-		public response: string,
-	) {
-		this.parseUrlVariables();
-		// remove trailing `/` from the path
-		this.url = this.url.replace(/\/$/, "").replace(/\/\?/, "?");
-		this.compiledUrlRegex = this.buildUrlRegex()
-	}
+  constructor(
+    public title: string,
+    public url: string,
+    public method: RequestMethod,
+    public accept: string,
+    public responseHeaders: Headers,
+    public response: string,
+  ) {
+    this.parseUrlVariables();
+    // remove trailing `/` from the path
+    this.url = this.url.replace(/\/$/, "").replace(/\/\?/, "?");
+    this.compiledUrlRegex = this.buildUrlRegex();
+  }
 
-	/**
+  /**
 	 * checks if the url matches our url rules
 	 * @param {string} url
 	 * @returns {boolean}
 	 */
-	public doesUrlMatch(url: string = ""): boolean {
-		url = url.toLowerCase();
-		// simple urls
-		return url === this.url || this.compiledUrlRegex.test(url)
-	}
+  public doesUrlMatch(url: string = ""): boolean {
+    url = url.toLowerCase();
+    // simple urls
+    return url === this.url || this.compiledUrlRegex.test(url);
+  }
 
-	/**
+  /**
 	 * parses our url and pulls out our parts of the url that should be variables
 	 * @private
 	 */
-	private parseUrlVariables() {
-		// matches a variable name after a /, and makes sure to not count a query variable as a flag for the var to be optional
-		const pathVarRegex = /(?<=\/:)[a-zA-Z_\-0-9]+(\?(?!=:))?/g;
-		// matches a query parameter variable name
-		const queryVarRegex = /(?<=[?&]:)[a-zA-Z_\-0-9]+\??/g;
-		// match and pull out our path variables
-		const matchedPathVars = this.url.match(pathVarRegex);
-		if(matchedPathVars) {
-			for(let pathVar of matchedPathVars) {
-				this.pathVariables.push(UrlVariable.fromString(pathVar));
-			}
-		}
-		// match and pull out our query variables
-		const matchedQueryVars = this.url.match(queryVarRegex);
-		if(matchedQueryVars) {
-			for(let queryVar of matchedQueryVars) {
-				this.queryVariables.push(UrlVariable.fromString(queryVar));
-			}
-		}
-	}
+  private parseUrlVariables() {
+    // matches a variable name after a /, and makes sure to not count a query variable as a flag for the var to be optional
+    const pathVarRegex = /(?<=\/:)[a-zA-Z_\-0-9]+(\?(?!=:))?/g;
+    // matches a query parameter variable name
+    const queryVarRegex = /(?<=[?&]:)[a-zA-Z_\-0-9]+\??/g;
+    // match and pull out our path variables
+    const matchedPathVars = this.url.match(pathVarRegex);
+    if (matchedPathVars) {
+      for (let pathVar of matchedPathVars) {
+        this.pathVariables.push(UrlVariable.fromString(pathVar));
+      }
+    }
+    // match and pull out our query variables
+    const matchedQueryVars = this.url.match(queryVarRegex);
+    if (matchedQueryVars) {
+      for (let queryVar of matchedQueryVars) {
+        this.queryVariables.push(UrlVariable.fromString(queryVar));
+      }
+    }
+  }
 
-	/**
+  /**
 	 * builds a regular expression used to match our url against potential candidates
 	 * @returns {RegExp}
 	 * @private
 	 */
-	private buildUrlRegex(): RegExp {
-		// replace all path variable names with a regex
-		let compiledUrlString = this.url;
-		// create variable regexes to match our url with
-		const nonOptionalPathRegex = /(?<=\/):(([a-zA-Z_\-0-9]+$)|([a-zA-Z_\-0-9]+(?=(\?:|\/))))/g;
-		const optionalPathRegex = /\/:[a-zA-Z_\-0-9]+\?(?!:)/g;
-		const nonOptionalQueryRegex = /(\\?)?[?&]:(([a-zA-Z_\-0-9]+$)|([a-zA-Z_\-0-9]+(?=&)))/g;
-		const optionalQueryRegex = /(\\?)?[?&]:[a-zA-Z_\-0-9]+(?=\?)\?/g;
-		// first replace any query string question marks since `?` is a special regex char 
-		compiledUrlString = compiledUrlString.replace(/\?:/g, '\\?:')
-		// for non-optional path param
-		compiledUrlString = compiledUrlString.replaceAll(
-			nonOptionalPathRegex,
-			"[a-zA-Z_\\-0-9]+",
-		);
-		// for optional path param
-		compiledUrlString = compiledUrlString.replaceAll(
-			optionalPathRegex,
-			"(/[a-zA-Z_\\-0-9]+)?",
-		);
-		// for non-optional query param
-		compiledUrlString = compiledUrlString.replaceAll(
-			nonOptionalQueryRegex,
-			"[?&][a-zA-Z_\\-0-9]+=[^&]+",
-		);
-		// for optional query param
-		compiledUrlString = compiledUrlString.replaceAll(
-			optionalQueryRegex,
-			"([?&][a-zA-Z_\\-0-9]+=[^&]+)?",
-		);
-		return new RegExp(`^${compiledUrlString}\$`)
-	}
+  private buildUrlRegex(): RegExp {
+    // replace all path variable names with a regex
+    let compiledUrlString = this.url;
+    // create variable regexes to match our url with
+    const nonOptionalPathRegex =
+      /(?<=\/):(([a-zA-Z_\-0-9]+$)|([a-zA-Z_\-0-9]+(?=(\?:|\/))))/g;
+    const optionalPathRegex = /\/:[a-zA-Z_\-0-9]+\?(?!:)/g;
+    const nonOptionalQueryRegex =
+      /(\\?)?[?&]:(([a-zA-Z_\-0-9]+$)|([a-zA-Z_\-0-9]+(?=&)))/g;
+    const optionalQueryRegex = /(\\?)?[?&]:[a-zA-Z_\-0-9]+(?=\?)\?/g;
+    // first replace any query string question marks since `?` is a special regex char
+    compiledUrlString = compiledUrlString.replace(/\?:/g, "\\?:");
+    // for non-optional path param
+    compiledUrlString = compiledUrlString.replaceAll(
+      nonOptionalPathRegex,
+      "[a-zA-Z_\\-0-9]+",
+    );
+    // for optional path param
+    compiledUrlString = compiledUrlString.replaceAll(
+      optionalPathRegex,
+      "(/[a-zA-Z_\\-0-9]+)?",
+    );
+    // for non-optional query param
+    compiledUrlString = compiledUrlString.replaceAll(
+      nonOptionalQueryRegex,
+      "[?&][a-zA-Z_\\-0-9]+=[^&]+",
+    );
+    // for optional query param
+    compiledUrlString = compiledUrlString.replaceAll(
+      optionalQueryRegex,
+      "([?&][a-zA-Z_\\-0-9]+=[^&]+)?",
+    );
+    return new RegExp(`^${compiledUrlString}\$`);
+  }
 
-	/**
+  /**
+	 * based on our url template, pulls variables out of the passed url and returns a basic JS object with the keys
+	 * as the variable name and the values as the variable value
+	 * @param {string} url
+	 * @returns {any}
+	 */
+  public parseVariablesFromUrl(url: string): any {
+    if (this.pathVariables.length === 0 && this.queryVariables.length === 0) {
+      return {};
+    }
+  }
+
+  /**
 	 * checks if this route has a path variable with the passed fields. Used for testing
 	 * @param {string} name
 	 * @param {boolean} optional
 	 * @returns {boolean}
 	 */
-	public hasPathVariable(name: string, optional: boolean): boolean {
-		return this.pathVariables.filter((it) =>
-			it.name === name && it.optional === optional
-		).length > 0;
-	}
+  public hasPathVariable(name: string, optional: boolean): boolean {
+    return this.pathVariables.filter((it) =>
+      it.name === name && it.optional === optional
+    ).length > 0;
+  }
 
-	/**
+  /**
 	 * checks if this route has a query variable with the passed fields. Used for testing
 	 * @param {string} name
 	 * @param {boolean} optional
 	 * @returns {boolean}
 	 */
-	public hasQueryVariable(name: string, optional: boolean): boolean {
-		return this.queryVariables.filter((it) =>
-			it.name === name && it.optional === optional
-		).length > 0;
-	}
+  public hasQueryVariable(name: string, optional: boolean): boolean {
+    return this.queryVariables.filter((it) =>
+      it.name === name && it.optional === optional
+    ).length > 0;
+  }
 
-	static fromObject(
-		// @ts-ignore
-		{title, url, method, accept, responseHeaders, response},
-	): Route {
-		const headers = this.createResponseHeaders(responseHeaders);
-		return new Route(
-			title,
-			url,
-			method,
-			accept,
-			headers,
-			response,
-		);
-	}
+  static fromObject(
+    // @ts-ignore
+    { title, url, method, accept, responseHeaders, response },
+  ): Route {
+    const headers = this.createResponseHeaders(responseHeaders);
+    return new Route(
+      title,
+      url,
+      method,
+      accept,
+      headers,
+      response,
+    );
+  }
 
-	/**
+  /**
 	 * creates a `Header` object from the raw input object. Conversion is done
 	 * by simple key/value pairs
 	 * @param input
 	 * @returns {Headers}
 	 * @private
 	 */
-	private static createResponseHeaders(input: any): Headers {
-		const headers = new Headers();
-		for(let [key, value] of Object.entries(input)) {
-			headers.set(key, String(value));
-		}
-		return headers;
-	}
+  private static createResponseHeaders(input: any): Headers {
+    const headers = new Headers();
+    for (let [key, value] of Object.entries(input)) {
+      headers.set(key, String(value));
+    }
+    return headers;
+  }
 }
